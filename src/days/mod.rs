8dae6a1dc96517ae045c980e2710e3773ea4a4f2
Mathis Burger <handymathis@gmail.com>
2024-12-01 12:22:18 +0100

    feat(framework): Added basic framework wrapper

---
+++ src/days/mod.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::Read;
+
+mod day1;
+
+pub fn run_day(day: u8) {
+    let file_path = "input.txt";
+    let mut file = File::open(file_path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    match day {
+        1 => day1::run(contents),
+        _ => println!("Day does not exist"),
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Write};
+
+use days::run_day;
+
+mod days;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Enter your day: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let day = input.trim().parse::<u8>().unwrap();
+    run_day(day);
 }
